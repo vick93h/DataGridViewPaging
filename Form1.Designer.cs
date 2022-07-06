ebf95038d70ec8edb43a14f6420435442c2bc7d6
vick93h <vittoriogerry@gmail.com>
2022-07-06 13:49:44 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace DataGridViewPaging
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnLatest = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.Display = new System.Windows.Forms.Label();
+            this.adventureWorks2019DataSet = new DataGridViewPaging.AdventureWorks2019DataSet();
+            this.storeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.storeTableAdapter = new DataGridViewPaging.AdventureWorks2019DataSetTableAdapters.StoreTableAdapter();
+            this.businessEntityIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.salesPersonIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.demographicsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rowguidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modifiedDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2019DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.storeBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.tableLayoutPanel2);
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 387);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 393);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(800, 57);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.businessEntityIDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.salesPersonIDDataGridViewTextBoxColumn,
+            this.demographicsDataGridViewTextBoxColumn,
+            this.rowguidDataGridViewTextBoxColumn,
+            this.modifiedDateDataGridViewTextBoxColumn});
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 2);
+            this.dataGridView1.DataSource = this.storeBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.tableLayoutPanel1.SetRowSpan(this.dataGridView1, 2);
+            this.dataGridView1.Size = new System.Drawing.Size(794, 381);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.tableLayoutPanel2.SetColumnSpan(this.panel2, 2);
+            this.panel2.Controls.Add(this.Display);
+            this.panel2.Controls.Add(this.btnLast);
+            this.panel2.Controls.Add(this.btnNext);
+            this.panel2.Controls.Add(this.btnPrevious);
+            this.panel2.Controls.Add(this.btnLatest);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.tableLayoutPanel2.SetRowSpan(this.panel2, 2);
+            this.panel2.Size = new System.Drawing.Size(794, 51);
+            this.panel2.TabIndex = 0;
+            // 
+            // btnLatest
+            // 
+            this.btnLatest.BackColor = System.Drawing.SystemColors.Window;
+            this.btnLatest.Location = new System.Drawing.Point(215, 19);
+            this.btnLatest.Name = "btnLatest";
+            this.btnLatest.Size = new System.Drawing.Size(75, 23);
+            this.btnLatest.TabIndex = 0;
+            this.btnLatest.Text = "|<<";
+            this.btnLatest.UseVisualStyleBackColor = false;
+            this.btnLatest.Click += new System.EventHandler(this.btnLatest_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.BackColor = System.Drawing.SystemColors.Window;
+            this.btnPrevious.Location = new System.Drawing.Point(296, 19);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 1;
+            this.btnPrevious.Text = "<<";
+            this.btnPrevious.UseVisualStyleBackColor = false;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.SystemColors.Window;
+            this.btnNext.Location = new System.Drawing.Point(455, 19);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = ">>";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.BackColor = System.Drawing.SystemColors.Window;
+            this.btnLast.Location = new System.Drawing.Point(536, 19);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 3;
+            this.btnLast.Text = ">>|";
+            this.btnLast.UseVisualStyleBackColor = false;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // Display
+            // 
+            this.Display.AutoSize = true;
+            this.Display.ForeColor = System.Drawing.SystemColors.Window;
+            this.Display.Location = new System.Drawing.Point(386, 24);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(0, 13);
+            this.Display.TabIndex = 4;
+            this.Display.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // adventureWorks2019DataSet
+            // 
+            this.adventureWorks2019DataSet.DataSetName = "AdventureWorks2019DataSet";
+            this.adventureWorks2019DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // storeBindingSource
+            // 
+            this.storeBindingSource.DataMember = "Store";
+            this.storeBindingSource.DataSource = this.adventureWorks2019DataSet;
+            // 
+            // storeTableAdapter
+            // 
+            this.storeTableAdapter.ClearBeforeFill = true;
+            // 
+            // businessEntityIDDataGridViewTextBoxColumn
+            // 
+            this.businessEntityIDDataGridViewTextBoxColumn.DataPropertyName = "BusinessEntityID";
+            this.businessEntityIDDataGridViewTextBoxColumn.HeaderText = "BusinessEntityID";
+            this.businessEntityIDDataGridViewTextBoxColumn.Name = "businessEntityIDDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // salesPersonIDDataGridViewTextBoxColumn
+            // 
+            this.salesPersonIDDataGridViewTextBoxColumn.DataPropertyName = "SalesPersonID";
+            this.salesPersonIDDataGridViewTextBoxColumn.HeaderText = "SalesPersonID";
+            this.salesPersonIDDataGridViewTextBoxColumn.Name = "salesPersonIDDataGridViewTextBoxColumn";
+            // 
+            // demographicsDataGridViewTextBoxColumn
+            // 
+            this.demographicsDataGridViewTextBoxColumn.DataPropertyName = "Demographics";
+            this.demographicsDataGridViewTextBoxColumn.HeaderText = "Demographics";
+            this.demographicsDataGridViewTextBoxColumn.Name = "demographicsDataGridViewTextBoxColumn";
+            // 
+            // rowguidDataGridViewTextBoxColumn
+            // 
+            this.rowguidDataGridViewTextBoxColumn.DataPropertyName = "rowguid";
+            this.rowguidDataGridViewTextBoxColumn.HeaderText = "rowguid";
+            this.rowguidDataGridViewTextBoxColumn.Name = "rowguidDataGridViewTextBoxColumn";
+            // 
+            // modifiedDateDataGridViewTextBoxColumn
+            // 
+            this.modifiedDateDataGridViewTextBoxColumn.DataPropertyName = "ModifiedDate";
+            this.modifiedDateDataGridViewTextBoxColumn.HeaderText = "ModifiedDate";
+            this.modifiedDateDataGridViewTextBoxColumn.Name = "modifiedDateDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2019DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.storeBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label Display;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnLatest;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private AdventureWorks2019DataSet adventureWorks2019DataSet;
+        private System.Windows.Forms.BindingSource storeBindingSource;
+        private AdventureWorks2019DataSetTableAdapters.StoreTableAdapter storeTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn businessEntityIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn salesPersonIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn demographicsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rowguidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn modifiedDateDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewPaging
+{
+    public partial class Form1 : Form
+    {
+        int pageNumber = 0;
+        int pageSize = 100;
+        public Form1()
+        {
+            InitializeComponent();
+            btnPrevious.Enabled = false;
+            btnLatest.Enabled = false;
+            this.storeTableAdapter.Fill(this.adventureWorks2019DataSet.Store);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var query = from c in adventureWorks2019DataSet.Store
+                        select new
+                        {
+                            BusinessEntityID = c.BusinessEntityID,
+                            Name = c.Name,
+                            SalesPersonID = c.SalesPersonID,
+                            Demographics = c.Demographics,
+                            rowguid = c.rowguid,
+                            ModifiedDate = c.ModifiedDate
+
+                        };
+
+            storeBindingSource.DataSource = query.Skip(pageSize * pageNumber).Take(pageSize).ToList();
+            btnPrevious.Enabled = false;
+            btnNext.Enabled = true;
+            btnLast.Enabled = true;
+            Display.Text = String.Format("Page {0}/{1}", pageNumber, query.Count() / pageSize);
+
+
+        }
+
+        private void btnLatest_Click(object sender, EventArgs e)
+        {
+
+            var query = from c in adventureWorks2019DataSet.Store
+                        select new
+                        {
+                            BusinessEntityID = c.BusinessEntityID,
+                            Name = c.Name,
+                            SalesPersonID = c.SalesPersonID,
+                            Demographics = c.Demographics,
+                            rowguid = c.rowguid,
+                            ModifiedDate = c.ModifiedDate
+
+                        };
+            pageNumber = 0;
+            storeBindingSource.DataSource = query.Skip(pageSize * pageNumber).Take(pageSize).ToList();
+            btnNext.Enabled = true;
+            btnPrevious.Enabled = false;
+            btnLatest.Enabled = !(pageNumber == 0);
+            btnLast.Enabled = true;
+            Display.Text = String.Format("Page {0}/{1}", pageNumber, query.Count() / pageSize);
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+
+            var query = from c in adventureWorks2019DataSet.Store
+                        select new
+                        {
+                            BusinessEntityID = c.BusinessEntityID,
+                            Name = c.Name,
+                            SalesPersonID = c.SalesPersonID,
+                            Demographics = c.Demographics,
+                            rowguid = c.rowguid,
+                            ModifiedDate = c.ModifiedDate
+
+                        };
+            pageNumber--;
+            storeBindingSource.DataSource = query.Skip(pageSize * pageNumber).Take(pageSize).ToList();
+
+            if (query.Skip(pageSize * (pageNumber - 1)).Take(pageSize).Count() > 0)
+                btnPrevious.Enabled = true;
+            else
+                btnPrevious.Enabled = false;
+            btnNext.Enabled = true;
+            btnLast.Enabled = true;
+            if (pageNumber == 0)
+            {
+                btnPrevious.Enabled = false;
+                btnLatest.Enabled = false;
+            }
+            else
+            {
+                btnPrevious.Enabled = true;
+                btnLatest.Enabled = true;
+            }
+            Display.Text = String.Format("Page {0}/{1}", pageNumber, query.Count() / pageSize);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+
+            var query = from c in adventureWorks2019DataSet.Store
+                        select new
+                        {
+                            BusinessEntityID = c.BusinessEntityID,
+                            Name = c.Name,
+                            SalesPersonID = c.SalesPersonID,
+                            Demographics = c.Demographics,
+                            rowguid = c.rowguid,
+                            ModifiedDate = c.ModifiedDate
+
+                        };
+            pageNumber++;
+            storeBindingSource.DataSource = query.Skip(pageSize * pageNumber).Take(pageSize).ToList();
+
+            if (query.Skip(pageSize * (pageNumber + 1)).Take(pageSize).Count() > 0)
+                btnNext.Enabled = true;
+            else
+                btnNext.Enabled = false;
+            btnPrevious.Enabled = true;
+            btnLatest.Enabled = true;
+            if (pageNumber == query.Count() / pageSize)
+            {
+                btnNext.Enabled = false;
+                btnLast.Enabled = false;
+            }
+            else
+            {
+                btnNext.Enabled = true;
+                btnLast.Enabled = true;
+            }
+
+            Display.Text = String.Format("Page {0}/{1}", pageNumber, query.Count() / pageSize);
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+
+            var query = from c in adventureWorks2019DataSet.Store
+                        select new
+                        {
+                            BusinessEntityID = c.BusinessEntityID,
+                            Name = c.Name,
+                            SalesPersonID = c.SalesPersonID,
+                            Demographics = c.Demographics,
+                            rowguid = c.rowguid,
+                            ModifiedDate = c.ModifiedDate
+
+                        };
+            pageNumber = query.Count() / (pageSize);
+            storeBindingSource.DataSource = query.Skip(pageSize * pageNumber).Take(pageSize).ToList();
+            btnNext.Enabled = false;
+            btnPrevious.Enabled = true;
+            btnLatest.Enabled = true;
+            btnLast.Enabled = !(pageNumber == query.Count() / pageSize);
+
+            Display.Text = String.Format("Page {0}/{1}", pageNumber, query.Count() / pageSize);
+        }
+    }
+}
